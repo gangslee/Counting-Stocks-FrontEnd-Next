bd42dac530904730b85236e609cf4fa126e70279
gangslee <grandnet1225@gmail.com>
2022-01-16 23:21:02 +0900

    Dev/Set next.config.js destination

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ module.exports = {
     return [
       {
         source: "/api/:path*",
-        destination: "https://api.example.com/:path*",
+        destination: "https://counting-stocks-front-end-6giol5n6o-gangslee.vercel.app/:path*",
       },
     ];
   },
