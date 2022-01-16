8868ad8162b1499f6ca87e014480a288334615a8
gangslee <grandnet1225@gmail.com>
2022-01-16 22:14:57 +0900

    deploy/Add Chart Library Dependencies Setting

--- utils/api.ts
+++ utils/api.ts
@@ -1,11 +1,11 @@
 import axios from "axios";
 
-// export const localApiGet = (url: string) =>
-//   axios.get(`http://localhost:8080/api/${url}`).then((res) => res.data);
 export const localApiGet = (url: string) =>
-  axios
-    .get(`https://counting-stocks-front-end-gangslee.vercel.app/api/${url}`)
-    .then((res) => res.data);
+  axios.get(`http://localhost:8080/api/${url}`).then((res) => res.data);
+// export const localApiGet = (url: string) =>
+//   axios
+//     .get(`https://counting-stocks-front-end-gangslee.vercel.app/api/${url}`)
+//     .then((res) => res.data);
 
 export const serverApi = axios.create({
   baseURL: process.env.REAL_SERVER,
