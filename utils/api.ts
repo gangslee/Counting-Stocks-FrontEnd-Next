26617111e4e6ec53b2d0849c317468800f6cb27b
gangslee <grandnet1225@gmail.com>
2022-01-16 22:07:18 +0900

    deploy/Set Axios URL Test

--- utils/api.ts
+++ utils/api.ts
@@ -1,8 +1,11 @@
 import axios from "axios";
 
+// export const localApiGet = (url: string) =>
+//   axios.get(`http://localhost:8080/api/${url}`).then((res) => res.data);
 export const localApiGet = (url: string) =>
-  axios.get(`http://localhost:8080/api/${url}`).then((res) => res.data);
-// const localApiGet = () => axios.get("https://counting-stocks-front-end-gangslee.vercel.app/api/").then((res) => res.data);
+  axios
+    .get(`https://counting-stocks-front-end-gangslee.vercel.app/api/${url}`)
+    .then((res) => res.data);
 
 export const serverApi = axios.create({
   baseURL: process.env.REAL_SERVER,
