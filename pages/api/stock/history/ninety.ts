aef7def0749096d905d9cdae1299dd4b2776c50d
gangslee <grandnet1225@gmail.com>
2022-01-16 23:27:39 +0900

    Fix/Set next-cors

--- pages/api/stock/history/ninety.ts
+++ pages/api/stock/history/ninety.ts
@@ -1,9 +1,8 @@
 import nc from "next-connect";
 import type { NextApiRequest, NextApiResponse } from "next";
 import yahooFinance from "yahoo-finance2";
-import { HistoricalOptions } from "yahoo-finance2/dist/esm/src/modules/historical";
+import NextCors from "nextjs-cors";
 import { getDay } from "../../../../utils/day";
-import { ModuleOptions } from "yahoo-finance2/dist/esm/src/lib/moduleCommon";
 
 const handler = nc<NextApiRequest, NextApiResponse>({
   onError: (err, req, res) => {
@@ -14,10 +13,17 @@ const handler = nc<NextApiRequest, NextApiResponse>({
     res.status(404).end("Page is not found - stock/ninety API");
   },
 }).get(async (req, res) => {
+  await NextCors(req, res, {
+    // Options
+    methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
+    origin: "*",
+    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+  });
+
   const today = new Date();
 
   const { symbol } = req.query;
-  const queryOptions: HistoricalOptions = { period1: getDay(-90), period2: today };
+  const queryOptions = { period1: getDay(-90), period2: today };
 
   const result = await yahooFinance.historical(`${symbol}`, queryOptions);
   const data = result.map((res) => {
