b68b20f8ad851ca20f9c64e92ebb280e004294fc
gangslee <grandnet1225@gmail.com>
2022-03-16 00:21:42 +0900

    [Dev] Version Up YahooFinance2 Because of API Issue

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,8 +10,9 @@ module.exports = {
       "CS_BLUE": "#5577dd",
       "CS_RED": "#dd4a4a",
       "BG_WHITE": "#f6f6f6",
+      "black": colors.black,
+      "white": colors.white,
       "zinc": colors.zinc,
-      "white": colors.white
     },
     fontFamily: {
       "s-core4": ["S-CoreDream-4Regular"],
